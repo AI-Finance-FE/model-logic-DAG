036356a8838e302ad0f90618d2adeaf961d854b7
wzanbaichi <czsylck@163.com>
2023-05-31 09:37:54 +0800

    style:格式化代码

--- src/index.js
+++ src/index.js
@@ -2,23 +2,23 @@
  * @Author: Liangchenkang
  * @Date: 2023-02-07 14:24:42
  * @Last Modified by: Liangchenkang
- * @Last Modified time: 2023-05-31 09:36:47
+ * @Last Modified time: 2023-05-31 09:37:29
  */
 import ModelDAG from './ModelDAG.vue'
 
 function flat(depth = 1) {
-return depth > 0
-  ? this.reduce((acc, val) => acc.concat(Array.isArray(val) ? val.flat(depth - 1) : val), [])
-  : Array.from(this);
+  return depth > 0
+    ? this.reduce((acc, val) => acc.concat(Array.isArray(val) ? val.flat(depth - 1) : val), [])
+    : Array.from(this)
 }
 
 // 将 flat 方法添加到 Array.prototype 上
 if (!Array.prototype.flat) {
-Object.defineProperty(Array.prototype, 'flat', {
-  value: flat,
-  writable: true,
-  configurable: true
-});
+  Object.defineProperty(Array.prototype, 'flat', {
+    value: flat,
+    writable: true,
+    configurable: true
+  })
 }
 
 export default ModelDAG
